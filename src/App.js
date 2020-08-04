9def50ec386010d63810c230086ebb1a53449e40
Juel <juel.marques-garcia@epitech.eu>
2020-08-04 12:00:13 +0200

    feat: setup create react app base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItem: 'center' }}><p>Farmers Dashboard</p></div>
+    </div>
+  );
+}
+
+export default App;
