034d741f362f83c4ed08b464fec1c9612494e066
Gregdams <gregoire.loupy@gmail.com>
2020-08-04 16:31:15 +0200

    [+](devops) add example test

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,8 +1,6 @@
 describe('My First Test', () => {
-    it('example button', () => {
+    it('example', () => {
       cy.visit('/')
-      cy.get('#btn').contains('0');
-      for (let i = 0; i != 10; i ++) cy.get('#btn').click()
-      cy.get('#btn').contains('10');
+      cy.contains('Farmers Dashboard')
     })
 })
