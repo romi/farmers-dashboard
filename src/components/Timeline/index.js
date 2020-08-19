fea74a6b6e2987c04ffcc255941a2ac61aa49773
Gregoire Loupy <gregoire.loupy@gmail.com>
2020-08-19 18:15:11 +0200

    [+](timeline-button) timeline chart redirect to /plant/:id

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
-
 import Chart from 'chart.js';
 
+import useRouter from '../../utils/hooks/router';
 import { FullLine } from './style';
 import { NoteContext } from '../../utils/providers/notes';
 
@@ -40,58 +40,60 @@ const options = {
   },
 };
 
-const data = {
-  labels: [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006],
-  datasets: [
-    {
-      fill: false,
-      backgroundColor: 'transparent',
-      pointRadius: 8,
-      pointHoverRadius: 8,
-      pointBorderWidth: 1,
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 3,
-      pointBackgroundColor: ['#00A960', '#00A960', '#00A960', '#00A960', '#FF8400', '#FF8400', '#6FE8AE'],
-      pointBorderColor: [],
-      data: [1, 1, 1, 1, 1, 1, 1],
-    },
-    {
-      fill: false,
-      backgroundColor: 'transparent',
-      borderColor: 'transparent',
-      pointRadius: 7,
-      pointHoverRadius: 7,
-      pointBorderWidth: 1,
-      pointHoverBorderColor: 'transparent',
-      pointHoverBorderWidth: 3,
-      pointBackgroundColor: [],
-      pointBorderColor: [],
-      data: [0, 0, 0, 0, 0, 0, 0],
-    },
-  ],
-};
-
-// eslint-disable-next-line no-unused-vars
 const Timeline = ({ scans }) => {
   const chartRef = useRef();
   let chart = useRef();
   const { isActive } = useContext(NoteContext);
+  const router = useRouter();
+
+  const data = {
+    labels: scans.map(({ date }) => date),
+    datasets: [
+      {
+        fill: false,
+        backgroundColor: 'transparent',
+        pointRadius: 8,
+        pointHoverRadius: 8,
+        pointBorderWidth: 1,
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 3,
+        pointBackgroundColor: scans.map(_ => '#00A960'),
+        // pointBackgroundColor: ['#00A960', '#00A960', '#00A960', '#00A960', '#FF8400', '#FF8400', '#6FE8AE'],
+        pointBorderColor: [],
+        data: scans.map(({ id, date }) => ({ y: 1, x: date, id })),
+      },
+      {
+        fill: false,
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        pointRadius: 7,
+        pointHoverRadius: 7,
+        pointBorderWidth: 1,
+        pointHoverBorderColor: 'transparent',
+        pointHoverBorderWidth: 3,
+        pointBackgroundColor: [],
+        pointBorderColor: [],
+        data: [0, 0, 0, 0, 0, 0, 0],
+      },
+    ],
+  };
 
   const clicked = useCallback(
     (_, i) => {
       const index = i[0]?._index;
-      if (typeof index !== 'number' || !chart || !isActive) return;
-      chart.data.datasets[1].pointBackgroundColor[index] =
-        chart.data.datasets[1].pointBackgroundColor[index] === '#d3d3d3' ? 'transparent' : '#d3d3d3';
-      chart.update();
+      if (isActive) {
+        if (typeof index !== 'number' || !chart) return;
+        chart.data.datasets[1].pointBackgroundColor[index] =
+          chart.data.datasets[1].pointBackgroundColor[index] === '#d3d3d3' ? 'transparent' : '#d3d3d3';
+        chart.update();
+      } else {
+        router.push(`/plant/${chart.data.datasets[0].data[index].id}`);
+      }
     },
-    [isActive],
+    [isActive, router],
   );
 
   useEffect(() => {
-    console.log(scans);
-    // To prevent infinite re-render
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     chart = new Chart(chartRef.current.getContext('2d'), {
       type: 'line',
       data,
