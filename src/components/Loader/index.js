9693a746d16cf56c14dd46343cc4fa0a9c617eb8
Juel <juel.marques-garcia@epitech.eu>
2020-08-27 17:41:16 +0200

    feat: add prop size in loading that allow the size parameter to be changed

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Loader from 'react-loader-spinner';
 import { theme } from 'utils/theme';
 import { Center } from 'components/PictureView/style';
 
-const Loading = () => (
+const Loading = ({ size }) => (
   <Center>
-    <Loader type="Oval" color={theme.primary} height={80} width={80} />
+    <Loader type="Oval" color={theme.primary} height={size} width={size} />
   </Center>
 );
+
+Loading.propTypes = {
+  size: PropTypes.number,
+};
+
+Loading.defaultProps = {
+  size: 80,
+};
 
 export default Loading;
