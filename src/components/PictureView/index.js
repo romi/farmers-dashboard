a490c623338c60c3db4b05a1702637bea11f720d
Gregoire Loupy <gregoire.loupy@gmail.com>
2020-09-08 15:07:44 +0200

    [~](dotted-line) refacto dotted line

--- src/components/PictureView/index.js
+++ src/components/PictureView/index.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState, useContext, useRef } from 'react';
+import React, { useState, useContext } from 'react';
 import { Line } from 'react-lineto';
 import PropTypes from 'prop-types';
 
-import useRouter from 'utils/hooks/router';
 import { ROMI_API } from 'utils/constants';
 import Button from 'components/Button';
-import { StageContext } from 'utils/providers/stage';
 import Loading from 'components/Loader';
+import { StageContext } from 'utils/providers/stage';
+import useRouter from 'utils/hooks/router';
+
+import { useRomiAnalyses } from './api';
 import {
   Center,
   Layout,
@@ -17,16 +19,14 @@ import {
   Thumbnail,
   ThumbnailInView,
 } from './style';
-import { useRomiAnalyses } from './api';
 
 export const PictureView = ({ imgData, plantData, scanId }) => {
-  const ref = useRef(null);
-  const router = useRouter();
-  const [blur, setBlur] = useState(false);
-  const { setPlantId } = useContext(StageContext);
   const [select, setSelect] = useState('picture');
   const { onRequest, viewOptions } = useRomiAnalyses(imgData, plantData.id);
+  const router = useRouter();
   const [currentPlant, setCurrentPlant] = useState({ id: -1, image: '' });
+  const { setPlantId } = useContext(StageContext);
+  const [bright, setBrightness] = useState(false);
   const [line, setLine] = useState({
     x0: 0,
     y0: 0,
@@ -34,55 +34,38 @@ export const PictureView = ({ imgData, plantData, scanId }) => {
     y1: 0,
   });
 
-  const resetValues = () => {
-    setCurrentPlant({ id: -1, image: '' });
-    setBlur(false);
-    setLine({ x0: 0, y0: 0, x1: 0, y1: 0 });
-  };
+  const clickEvent = evt => {
+    const boardPic = document.getElementById('board-picture');
+    const thumb = document.getElementById('thumbnail');
+    const ratioX = parseFloat(viewOptions.width / (evt.target.width + boardPic.scrollLeftMax));
+    const ratioY = parseFloat(viewOptions.height / (evt.target.height + boardPic.scrollTopMax));
+    const x = (evt.clientX + boardPic.scrollLeft - boardPic.offsetLeft) * ratioX;
+    const y = (evt.clientY + boardPic.scrollTop - boardPic.offsetTop) * ratioY;
 
-  useEffect(() => {
-    const clickEvent = evt => {
-      if (ref.current && !ref.current.contains(evt.target)) {
-        resetValues();
-        return;
-      }
-      const boardPic = document.getElementById('board-picture');
-      const ratioX = parseFloat(viewOptions.width / (evt.target.width + boardPic.scrollLeftMax));
-      const ratioY = parseFloat(viewOptions.height / (evt.target.height + boardPic.scrollTopMax));
-      const x = (evt.clientX + boardPic.scrollLeft - boardPic.offsetLeft) * ratioX;
-      const y = (evt.clientY + boardPic.scrollTop - boardPic.offsetTop) * ratioY;
+    const plant = viewOptions.plants.find(
+      ({ x: px, y: py, width, height }) => x >= px - width && x <= px + width && y >= py - height && y <= py + height,
+    );
 
-      const { offsetTop, offsetLeft, offsetWidth, offsetHeight } = document.getElementById('thumbnail');
-      const plant = viewOptions.plants.find(
-        ({ x: px, y: py, width, height }) => x >= px - width && x <= px + width && y >= py - height && y <= py + height,
-      );
-
-      if (!plant) return;
-      setCurrentPlant({
-        ...plant,
-        x: plant.x / ratioX,
-        y: (plant.y - plant.height / 2) / ratioY,
-        width: plant.width / ratioX,
-        height: plant.height / ratioY,
+    if (!plant) return;
+    setCurrentPlant({
+      ...plant,
+      x: plant.x / ratioX,
+      y: (plant.y - plant.height / 2) / ratioY,
+      width: plant.width / ratioX,
+      height: plant.height / ratioY,
+    });
+    if (!router.pathname.includes('plant')) router.push(`/plant/${scanId}`);
+    else {
+      setPlantId(plant.id);
+      setLine({
+        x0: thumb.offsetLeft + thumb.offsetWidth,
+        y0: thumb.offsetTop + thumb.offsetHeight / 2,
+        x1: plant.x / ratioX + boardPic.scrollLeft + boardPic.offsetLeft,
+        y1: plant.y / ratioY + boardPic.scrollTop + boardPic.offsetTop,
       });
-      if (!router.pathname.includes('plant')) router.push(`/plant/${scanId}`);
-      else {
-        setPlantId(plant.id);
-        setLine({
-          x0: offsetLeft + offsetWidth,
-          y0: offsetTop + offsetHeight / 2,
-          x1: plant.x / ratioX + boardPic.scrollLeft + boardPic.offsetLeft,
-          y1: plant.y / ratioY + boardPic.scrollTop + boardPic.offsetTop,
-        });
-        setBlur(true);
-      }
-    };
-
-    document.addEventListener('mousedown', clickEvent);
-    return () => {
-      document.removeEventListener('mousedown', clickEvent);
-    };
-  }, [ref, viewOptions, scanId, setPlantId, router]);
+      setBrightness(true);
+    }
+  };
 
   if (onRequest) return <Loading />;
   if (!imgData || !viewOptions) return <Center>There is no image or plant analyses of the board</Center>;
@@ -93,10 +76,10 @@ export const PictureView = ({ imgData, plantData, scanId }) => {
       )}
       <Layout>
         <ButtonList>
-          <ThumbnailContainer id="thumbnail" style={{ opacity: currentPlant?.image ? 1 : 0 }}>
+          <ThumbnailContainer id="thumbnail" show={currentPlant?.image}>
             <Thumbnail
-              style={{ display: currentPlant?.image ? 'unset' : 'hidden' }}
               alt="Selected Plant"
+              show={currentPlant?.image}
               src={`${ROMI_API}/images/${currentPlant?.image}?size=thumb&orientation=horizontal&direction=ccw`}
             />
           </ThumbnailContainer>
@@ -107,17 +90,20 @@ export const PictureView = ({ imgData, plantData, scanId }) => {
             Inspection
           </Button>
         </ButtonList>
-        <ImgContainer id="board-picture" ref={ref}>
-          {blur && (
+        <ImgContainer id="board-picture" onClick={clickEvent}>
+          {bright && (
             <ThumbnailInView
               alt="thumbnail-view"
-              {...currentPlant}
+              x={currentPlant?.x}
+              y={currentPlant?.y}
+              width={currentPlant?.width}
+              height={currentPlant?.height}
               src={`${ROMI_API}/images/${currentPlant?.image}?size=thumb&orientation=horizontal&direction=ccw`}
             />
           )}
           <Image
             alt="board picture"
-            style={{ filter: blur && 'brightness(0.5)' }}
+            brightness={bright}
             src={`${ROMI_API}/images/${viewOptions.options[select]}?size=large&orientation=horizontal&direction=ccw`}
           />
         </ImgContainer>
